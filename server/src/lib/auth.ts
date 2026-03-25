b830d6f6cd77efa3fe6988bfe10579b57be36c52
Nazar <nazar1337@MacBook-Air-Nazar.local>
2026-03-25 19:12:21 +0200

    init

---
+++ server/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { betterAuth } from "better-auth";
+import { openAPI } from "better-auth/plugins";
+
+export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL!,
+    trustedOrigins: [process.env.TRUSTED_ORIGIN!],
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema,
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    user: {
+        deleteUser: {
+            enabled: true,
+        },
+    },
+    plugins: [openAPI()],
+});
